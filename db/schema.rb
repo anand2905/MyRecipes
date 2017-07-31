072978cb6b7fc846f9875d4b2b4cc38c1c060500
anand2905 <anand29.ku@gmail.com>
2017-07-30 22:51:58 -0700

    create chefs resourecs and add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729175631) do
+ActiveRecord::Schema.define(version: 20170730063125) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "likes", force: :cascade do |t|
